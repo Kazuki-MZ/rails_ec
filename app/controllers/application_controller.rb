eb9ddafc006ec47ffac75be6327bf8ab9c7a4594
kazu1212-star <12matumatu04@gmail.com>
2023-03-18 23:56:31 +0900

    nilが返った場合を考慮したコードに修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,10 +7,10 @@ class ApplicationController < ActionController::Base
 
   def current_cart
     if session[:cart_id]
-      @current_cart ||= Cart.find_by(id: session[:cart_id])
+      @current_cart ||= Cart.find_by!(id: session[:cart_id])
 
     else
-      @current_cart = Cart.create
+      @current_cart = Cart.create!
       session[:cart_id] = @current_cart.id
     end
     @current_cart

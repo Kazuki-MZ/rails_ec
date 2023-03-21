c23204c19600162bf132c8a130ec8bf3940386f6
kazu1212-star <12matumatu04@gmail.com>
2023-03-21 22:19:27 +0900

    ロジックを細かく分割

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -4,13 +4,7 @@ class CartItemsController < ApplicationController
   before_action :set_cart_item, only: %i[destroy]
 
   def create
-    cart_item = current_cart.cart_items.find_by(item_id: params[:item_id])
-    if cart_item
-      cart_item.quantity = cart_item.quantity.to_i + params[:quantity].to_i
-      cart_item.save!
-    else
-      current_cart.cart_items.build(item_id: params[:item_id], quantity: params[:quantity]).save!
-    end
+    add_item_to_cart
     redirect_to item_cart_items_path
   end
 
@@ -20,6 +14,24 @@ class CartItemsController < ApplicationController
   end
 
   private
+
+  def add_item_to_cart
+    cart_item = current_cart.cart_items.find_by(item_id: params[:item_id])
+    if cart_item
+      update_cart_item(cart_item)
+    else
+      create_new_cart_item
+    end
+  end
+
+  def update_cart_item(cart_item)
+    cart_item.quantity = cart_item.quantity.to_i + params[:quantity].to_i
+    cart_item.save!
+  end
+
+  def create_new_cart_item
+    current_cart.cart_items.build(item_id: params[:item_id], quantity: params[:quantity]).save!
+  end
 
   def set_cart_item
     @my_cart_item = current_cart.cart_items.find(params[:id])

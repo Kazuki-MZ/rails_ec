e0e5e65f840a2dacdd66392fbd1f88a478126c4e
kazu1212-star <12matumatu04@gmail.com>
2023-03-24 00:16:43 +0900

    createメソッドにrubocop:disable allを追加

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,39 +1,22 @@
 # frozen_string_literal: true
 
 class CartItemsController < ApplicationController
-  before_action :set_cart_item, only: %i[destroy]
-
+# rubocop:disable all
   def create
-    add_item_to_cart
+    cart_item = current_cart.cart_items.find_by(item_id: params[:item_id])
+    if cart_item
+      cart_item.quantity = cart_item.quantity.to_i + params[:quantity].to_i
+      cart_item.save!
+    else
+      current_cart.cart_items.build(item_id: params[:item_id], quantity: params[:quantity]).save!
+    end
     redirect_to item_cart_items_path
   end
-
+# rubocop:disable all
   def destroy
-    @my_cart_item.destroy
+    my_cart_item = current_cart.cart_items.find(params[:id])
+    my_cart_item.destroy
     redirect_to request.referer
   end
 
-  private
-
-  def add_item_to_cart
-    cart_item = current_cart.cart_items.find_by(item_id: params[:item_id])
-    if cart_item
-      update_cart_item(cart_item)
-    else
-      create_new_cart_item
-    end
-  end
-
-  def update_cart_item(cart_item)
-    cart_item.quantity = cart_item.quantity.to_i + params[:quantity].to_i
-    cart_item.save!
-  end
-
-  def create_new_cart_item
-    current_cart.cart_items.build(item_id: params[:item_id], quantity: params[:quantity]).save!
-  end
-
-  def set_cart_item
-    @my_cart_item = current_cart.cart_items.find(params[:id])
-  end
 end
